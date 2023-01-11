7be780d501445104f451d1a9bbcf91cd0aa78d1c
Chai <83523587+CPScript@users.noreply.github.com>
2023-01-11 13:57:28 -0500

    Create Naming Standards.c

---
+++ Variables/Naming Standards.c
@@ -0,0 +1,10 @@
+# We are going to start with the second part of our variable declaration, type variable_name, the variable_name. C does not allow you to throw anything down and call it a variable name, there are some restrictions.
+
+# Names can be composed of upper and lower case letters, numbers, and underscores.
+# The first character must be a letter (upper or lower case).
+# No keywords are allowed as the full name (int is not allowed but int_count would work).
+# That’s it, nothing too crazy, just keep those rules in mind when you are creating your names for your variables
+
+#Note...
+# this is getting a bit easyer
+
