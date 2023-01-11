1c6cb57eb94365a9546ac9db246717d5452a2d71
Chai <83523587+CPScript@users.noreply.github.com>
2023-01-11 13:19:51 -0500

    Create 4 Comments.c

---
+++ Hello world/4 Comments.c
@@ -0,0 +1,43 @@
+# When we write code it is important to document the code’s behavior. One way to do this is to add comments to our code.
+# Starting a line with a double forward slash, //, will create a comment and the entire line will be ignored when we run the code.
+
+// This is a single line comment
+ 
+// This is 2 single line comments 
+// together to explain a little more
+
+# The comments like the ones above can be added above a line or block of code to describe the code’s behavior. 
+# Shorter comments can be added to the end of a line of code as well.
+
+printf("My dog is happy!"); // How my dog feels
+
+# In both examples, once you use // the rest of the line is now a comment.
+
+# If you want to create a comment with a beginning and end, 
+# you can use /* to begin the comment and */ to end the comment. This is known as a block comment:
+
+/* The following output will be
+an outburst from my dog in a 
+moment of pure joy after seeing 
+another dog across the street. */
+printf("Woof!");
+
+# As you can see from the above example a block comment can wrap multiple lines without the use of anything but the beginning notation
+/* and the ending notation */.
+  
+ 
+  
+  
+  
+  
+  ==========
+  #include <stdio.h>
+
+int main() {
+  //C CODE IS SO ANOYING :)//
+  printf("Chocolate chip cookies are way better than oatmeal raisin cookies.\n");
+
+  /*bruvv*/
+  printf("Learning the C programming language is an exciting adventure!\n");
+} 
+  
